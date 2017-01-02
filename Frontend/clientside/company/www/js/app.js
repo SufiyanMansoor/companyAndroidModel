069369634faea03793fc934de51269386a3de480
SufiyanMansoor <sufiyanbaw1256@gmail.com>
2017-01-02 10:06:23 +0500

    upadted new routes and setting apps

--- Frontend/clientside/company/www/js/app.js
+++ Frontend/clientside/company/www/js/app.js
@@ -4,6 +4,73 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('intro', {
+        url: '/',
+        templateUrl: 'templates/intro/intro.html',
+        controller: 'IntroCtrl'
+      })
+      .state('main', {
+        url: '/main',
+        templateUrl: 'templates/main/main.html',
+        controller: 'MainCtrl'
+      })
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home/home.html',
+      controller: 'HomeCtrl'
+    });
+    $urlRouterProvider.otherwise("/");
+
+  })
+
+  .controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+
+    // Called to navigate to the main app
+    $scope.startApp = function() {
+      $state.go('main');
+    };
+    $scope.next = function() {
+      $ionicSlideBoxDelegate.next();
+    };
+    $scope.previous = function() {
+      $ionicSlideBoxDelegate.previous();
+    };
+
+    // Called each time the slide changes
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
+    };
+  })
+
+  .controller('MainCtrl', function($scope, $state) {
+    console.log('MainCtrl');
+
+    $scope.toIntro = function(){
+      $state.go('intro');
+    }
+  })
+  .controller('HomeCtrl', function($scope, $state) {
+    console.log('MainCtrl');
+
+    $scope.toIntro = function(){
+      $state.go('intro');
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
